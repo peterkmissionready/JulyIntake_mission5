3dfbaecd9c8c6c016ced6db14859c620cb8490e5
peterkmissionready <peterk@missionreadyhq.com>
2023-09-22 18:01:28 +1200

    trying to fix no such remote origin

--- backend/api/someData.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import MyDataCtrl from './myData.controller.js';
-
-const router = express.Router();
-
-router.route('/').get(MyDataCtrl.apiGetMyData);
-
-export default router;
--- frontend/src/services/myData.js
+++
@@ -1,9 +0,0 @@
-import http from "../http-common";
-
-class MyDataService {
-    getAll(page = 0) {
-        return http.get(`?page=${page}`);
-    }
-}
-
-export default new MyDataService();
